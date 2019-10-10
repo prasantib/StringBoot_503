1574add9ced2df65555b15ed2b9ba8ad9e0c9bdb
prasantib <prasanti.babu@gmail.com>
2019-10-10 11:51:48 -0400

    StringBoot_503

---
+++ src/main/java/com/example/demo/StringBoot503Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot503Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot503Application.class, args);
+    }
+
+}
